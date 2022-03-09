1498d68c3abddba0ef904c329b39745bebbc711e
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2022-03-09 14:54:48 +0100

    refactor: rename the pipeline name

--- pipelines/kibana-performance-daily.tf
+++ pipelines/kibana-performance-daily.tf
@@ -1,5 +1,5 @@
-resource "buildkite_pipeline" "performance_daily" {
-  name        = "kibana / performance daily"
+resource "buildkite_pipeline" "single_user_performance_daily" {
+  name        = "kibana / single user performance daily"
   description = "Runs performance tests daily"
   repository  = "https://github.com/elastic/kibana"
   steps       = <<-EOT
@@ -16,12 +16,11 @@ resource "buildkite_pipeline" "performance_daily" {
   branch_configuration = join(" ", local.current_dev_branches)
 }
 
-resource "buildkite_pipeline_schedule" "performance_daily_ci" {
+resource "buildkite_pipeline_schedule" "single_user_performance_daily_ci" {
   for_each = toset(local.current_dev_branches)
 
-  pipeline_id = buildkite_pipeline.performance_daily.id
-  label       = "Daily build"
-  cronline    = "0 9 * * * Europe/Berlin"
+  pipeline_id = buildkite_pipeline.single_user_performance_daily.id
+  label       = "Single user daily test"
   branch      = each.value
   env         = {
     PERF_TEST_COUNT="100"
