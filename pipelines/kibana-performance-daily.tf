c754ec75bfd256dd387aee8b209236f56b9c2d93
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2022-03-09 20:11:33 +0100

    refactor: pipeline naming and remove already set env var

--- pipelines/kibana-performance-daily.tf
+++ pipelines/kibana-performance-daily.tf
@@ -1,5 +1,5 @@
 resource "buildkite_pipeline" "single_user_performance" {
-  name        = "kibana-single-user-performance"
+  name        = "kibana / single-user-performance"
   description = "Runs single user performance tests for kibana"
   repository  = "https://github.com/elastic/kibana"
   steps       = <<-EOT
@@ -22,6 +22,5 @@ resource "buildkite_pipeline_schedule" "single_user_performance_daily" {
   cronline    = "0 * * * * Europe/Berlin"
   branch      = buildkite_pipeline.single_user_performance.default_branch
   env         = {
-    TEST_BROWSER_HEADLESS=1
   }
 }
