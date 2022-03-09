480b34e7dbbc3b7b2f3e205d3acd3a8117dd13b8
Cavit Baturalp Gurdin <baturalp.gurdin@elastic.co>
2022-03-09 15:02:40 +0100

    ci: only run on main branch and rename pipeline

--- pipelines/kibana-performance-daily.tf
+++ pipelines/kibana-performance-daily.tf
@@ -1,6 +1,6 @@
-resource "buildkite_pipeline" "single_user_performance_daily" {
-  name        = "kibana / single user performance daily"
-  description = "Runs performance tests daily"
+resource "buildkite_pipeline" "single_user_performance" {
+  name        = "kibana-single-user-performance"
+  description = "Runs single user performance tests for kibana"
   repository  = "https://github.com/elastic/kibana"
   steps       = <<-EOT
   env:
@@ -13,16 +13,14 @@ resource "buildkite_pipeline" "single_user_performance_daily" {
   EOT
 
   default_branch       = "main"
-  branch_configuration = join(" ", local.current_dev_branches)
+  branch_configuration = "main"
 }
 
-resource "buildkite_pipeline_schedule" "single_user_performance_daily_ci" {
-  for_each = toset(local.current_dev_branches)
-
-  pipeline_id = buildkite_pipeline.single_user_performance_daily.id
+resource "buildkite_pipeline_schedule" "single_user_performance_daily" {
+  pipeline_id = buildkite_pipeline.single_user_performance.id
   label       = "Single user daily test"
   cronline    = "0 * * * * Europe/Berlin"
-  branch      = each.value
+  branch      = buildkite_pipeline.single_user_performance.default_branch
   env         = {
     TEST_BROWSER_HEADLESS=1
   }
